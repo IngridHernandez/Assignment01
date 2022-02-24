50b2cd730156f7c203fa48fc65390da28f4b05c1
IngridHernandez <60713446+IngridHernandez@users.noreply.github.com>
2022-02-23 23:33:48 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,73 @@
+ //step0 https://github.com/IngridHernandez/Assignment01.git
+ //step1
+ //let someMonth; 
+ //function theMonth();
+ //let currentMonth;
+ //let summerMonth;
+ //let myLibraryFunction;/
+
+ //step2
+// 3.9 // numeric literal expression
+// "hello!" //string literal expression
+//false //a Boolean literal expression,
+//null // null literal expression.
+
+ //step3 complex variable expression
+// var anExpression = 3 * (4 / 5) + 6;
+// var aSecondExpression = Math.PI * radius * radius;
+
+ //step4 Declare (but do not assign) 9 variables
+//  var firstName;
+//  var lastName;
+//  var Address; 
+//  var City;
+//  var State; 
+//  var ZipCode; 
+//  var YouAge;
+//  var May , We , Contact , You;
+
+ //step5 
+ /*
+ demonstrate 3 ways for declaring and assigning values to 
+those variables.
+*/
+// var firstName = "Ingrid";
+//  var lastName = "Hernandez";
+//  var YouAge = "53";
+
+//var firstName;
+//firstName = "Ingrid";
+//  var lastName;
+//  lastName = "Hernandez";
+//  var yourAge;
+//  yourAge = "53"
+
+//  var myName = "Ingrid Hernandez", myAge = 53;
+
+ //step6
+ /*
+ Create a variable. 
+Add a number and a string and display the coerced 
+result in the browser’s console window. 
+*/
+// var yearsWorking = 30;
+// window.console.log("Ingrid has been working for" + yearsWorking + "years.");
+
+//step07
+
+//step08
+// let someString = "Who once said, 'Only two things are infinite', 'the universe 
+// and 'human stupidity', and I'm not sure about the former.";
+// window.console.log(someString);
+
+//09
+//10
+//11
+ //alert("Hello Ingrid Hernandez, + welcome to the JavaScript class!");//It worked
+//12
+//var name = "ingrid";
+//13
+// var course = "javaScript";
+// alert("course, javaScript");
+
+
